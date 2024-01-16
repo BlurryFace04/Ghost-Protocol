38850f4ab022a74f48ab645124af2715dfc1ae8e
BlurryFace04 <arnav29104@gmail.com>
2024-01-16 22:54:56 +0530

    connectkit init

---
+++ components/provider.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from 'next-themes'
+import { ThemeProviderProps } from 'next-themes/dist/types'
+import { EthereumProvider } from "./ethereum";
+
+export function Provider({ children, ...props }: ThemeProviderProps) {
+  return (
+    <ThemeProvider {...props}>
+      <EthereumProvider>
+        {children}
+      </EthereumProvider>
+    </ThemeProvider>
+  );
+}
